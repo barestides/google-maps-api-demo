291c57b7b32f39f9a8aafa1576b1903a9852fed7
Braden Arestides <barestides@gmail.com>
2018-11-21 16:54:44 -0500

    Add map to page.
    Include google maps script.
    Make app state global rather than passing it around everywhere.
    Add custom css stylesheet.

--- src/google_maps_api_demo/core.cljs
+++ src/google_maps_api_demo/core.cljs
@@ -11,49 +11,69 @@
                             :destination ""}))
 
 
+;;These contain JS objects that are initialized when the page is loaded
+(def directions-service-atom (atom nil))
+(def directions-display-atom (atom nil))
+
 (defn titleize
   [str-or-kw]
   (string/capitalize (name str-or-kw)))
 
 (defn text-box-on-change-fn
-  [state field]
+  [field]
   (fn [new-val]
-    (swap! state assoc field (-> new-val
+    (swap! app-state assoc field (-> new-val
                                  .-target
                                  .-value))))
 
 (defn text-box
-  [state field]
+  [field]
   [:div.form-group
    [:label {:for field} (titleize field)]
-   [:input.form-control {:id field :type "text" :value (field @state)
-                                 :on-change (text-box-on-change-fn state field)}]])
+   [:input.form-control {:id field :type "text" :value (field @app-state)
+                                 :on-change (text-box-on-change-fn field)}]])
 
 (defn submit-form
-  [state]
+  []
   (prn "we submitting"))
 
 (defn location-form
-  [state]
+  []
   [:form.col-sm-6 {:on-submit (fn [event]
                                 ;;so the page doesn't refresh when the user submits
                                 (do
                                   (.preventDefault event)
-                                  (submit-form state)))}
-   [text-box state :origin]
-   [text-box state :destination]
+                                  (submit-form)))}
+   [text-box :origin]
+   [text-box :destination]
    [:input.btn.btn-primary {:type :submit :value "Submit"}]])
 
+(defn init-map
+  []
+  (let [directions-service (google.maps.DirectionsService.)
+        directions-display (google.maps.DirectionsRenderer.)
+        map (google.maps.Map. (.getElementById js/document "map")
+                 (clj->js {:zoom 7
+                           :center {:lat 33.74 :lng -84.38}}))]
+    (.setMap directions-display map)
+    (reset! directions-service-atom directions-service)
+    (reset! directions-display-atom directions-display)))
+
+(defn map-elm
+  []
+  (r/create-class
+   {:reagent-render (fn [] [:div#map.col-sm-6])
+    :component-did-mount (fn [this] (init-map))}))
 
 (defn page
-  [state]
+  []
   [:div.container
    [:h2 "Google Maps API CLJS Example App"]
-   [:div.row
-    [location-form state]]])
+   [:div.row [location-form]]
+   [:div.row [map-elm]]])
 
 (defn on-js-reload []
-  ;; optionally touch your app-state to force rerendering depending on
+  ;; optionally touch your app-app-state to force rerendering depending on
   ;; your application
-  ;; (swap! app-state update-in [:__figwheel_counter] inc)
-  (r/render [page app-state] (.getElementById js/document "app")))
+  ;; (swap! app-app-state update-in [:__figwheel_counter] inc)
+  (r/render [page] (.getElementById js/document "app")))
